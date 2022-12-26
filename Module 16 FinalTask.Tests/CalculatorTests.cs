adf390ba3324e8380774b318e4f0d384fe1aa855
Nikula Daniil <daniil.nikula@gmail.com>
2022-12-26 14:22:07 +0200

    Добавьте файлы проекта.

---
+++ Module 16 FinalTask.Tests/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Module_16_FinalTask.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(29, 10) == 290);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
---
+++ Module 16 FinalTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_16_FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
